6f204d5e47014e69cf8757b8b40e349843f7c245
joshgoldeneagle <joshuabgold@gmail.com>
2015-05-13 11:37:48 -0700

    added error checking for sign in

--- app/src/main/java/com/joshbgold/ribbit/RibbitApplication.java
+++ app/src/main/java/com/joshbgold/ribbit/RibbitApplication.java
@@ -3,7 +3,6 @@ package com.joshbgold.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 public class RibbitApplication extends Application {
 
@@ -14,8 +13,9 @@ public class RibbitApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "W2tNAzL0KmSX6D2KzPrgXj6xVWmDHhyU0oeCuqAd", "31zsSt6SutjBLLnJVgrPith4XyGwHbPSsQTjF4wZ");
 
+     /* //For testing
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        testObject.saveInBackground();*/
     }
 }
