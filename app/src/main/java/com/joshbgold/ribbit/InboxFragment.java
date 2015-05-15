51fa70447545f04037f11f4cb840220bd6d9c4b9
joshgoldeneagle <joshuabgold@gmail.com>
2015-05-15 15:47:47 -0700

    added fragments

---
+++ app/src/main/java/com/joshbgold/ribbit/InboxFragment.java
@@ -0,0 +1,22 @@
+package com.joshbgold.ribbit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by JoshG on 5/15/2015.
+ */
+public class InboxFragment extends android.support.v4.app.ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox,
+                container, false);
+
+        return rootView;
+    }
+
+}
