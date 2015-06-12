2eea5d2f60b8bb2f1aa8da822ebb597661ad7a18
joshgoldeneagle <joshuabgold@gmail.com>
2015-06-12 13:14:31 -0700

    new stuff

--- app/src/main/java/com/joshbgold/ribbit/InboxFragment.java
+++ app/src/main/java/com/joshbgold/ribbit/InboxFragment.java
@@ -5,10 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 /**
  * Created by JoshG on 5/15/2015.
  */
-public class InboxFragment extends android.support.v4.app.ListFragment {
+public class InboxFragment extends android.app.ListFragment {
+
+    protected  List<ParseObject> mMessages;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,5 +28,40 @@ public class InboxFragment extends android.support.v4.app.ListFragment {
 
         return rootView;
     }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENTS_IDS, ParseUser.getCurrentUser().getObjectId());
+        query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+                if (e == null) {
+                    // We found messages!
+                    mMessages = messages;
+
+                    String[] usernames = new String[mMessages.size()];
+                    int i = 0;
+                    for(ParseObject message : mMessages) {
+                        usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+                    messageAdapter adapter = new messageAdapter(
+                            getListView().getContext(),
+                            mMessages);
+                    setListAdapter(adapter);
+                }
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/joshbgold/ribbit/MainActivity.java
+++ app/src/main/java/com/joshbgold/ribbit/MainActivity.java
@@ -210,6 +210,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
