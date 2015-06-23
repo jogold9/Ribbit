50c9d3ae8512dc259e4684404c0b259a5259a890
joshgoldeneagle <joshuabgold@gmail.com>
2015-06-22 17:28:58 -0700

    new stuff

---
+++ app/src/main/java/com/joshbgold/ribbit/ViewImageActivity.java
@@ -0,0 +1,52 @@
+package com.joshbgold.ribbit;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class ViewImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+        //Show the Up button in the action bar
+        setupActionBar();
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        Uri imageUri = getIntent().getData();
+
+    }
+
+    //Set up the @link android.app.ActionBar
+    private void setupActionBar(){
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_image, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
