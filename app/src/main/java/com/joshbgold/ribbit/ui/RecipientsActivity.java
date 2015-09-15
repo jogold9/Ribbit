a334a084cf721106d40bab512c3f050ae885632d
joshgoldeneagle <joshuabgold@gmail.com>
2015-09-14 17:24:27 -0700

    finished changing listviews to gridviews in FriendsFragment and EditFriendsActivity

--- app/src/main/java/com/joshbgold/ribbit/ui/RecipientsActivity.java
+++ app/src/main/java/com/joshbgold/ribbit/ui/RecipientsActivity.java
@@ -1,7 +1,7 @@
 package com.joshbgold.ribbit.ui;
 
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -10,13 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.joshbgold.ribbit.R;
+import com.joshbgold.ribbit.adapters.UserAdapter;
 import com.joshbgold.ribbit.utils.FileHelper;
 import com.joshbgold.ribbit.utils.ParseConstants;
-import com.joshbgold.ribbit.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -30,7 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RecipientsActivity extends ListActivity{
+public class RecipientsActivity extends Activity {
 
     public static final String TAG = RecipientsActivity.class.getSimpleName();
 
@@ -40,6 +44,7 @@ public class RecipientsActivity extends ListActivity{
     protected MenuItem mSendMenuItem;
     protected Uri mMediaUri;
     protected String mFileType;
+    protected GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +52,15 @@ public class RecipientsActivity extends ListActivity{
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.user_grid);
 
-        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        mGridView = (GridView)findViewById(R.id.friendsGrid);
+        mGridView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        mGridView.setOnItemClickListener(mOnItemClickListener);
 
         mMediaUri = getIntent().getData();
         mFileType = getIntent().getExtras().getString(ParseConstants.KEY_FILE_TYPE);
+
+        TextView emptyTextView = (TextView)findViewById(android.R.id.empty);
+        mGridView.setEmptyView(emptyTextView);
     }
 
     @Override
@@ -78,10 +88,15 @@ public class RecipientsActivity extends ListActivity{
                         usernames[i] = friend.getUsername();
                         i++;
                     }
-                    ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                            getListView().getContext(),
-                            android.R.layout.simple_list_item_checked, usernames);
-                    setListAdapter(adapter);
+
+                    if (mGridView.getAdapter() == null) {
+                        UserAdapter adapter = new UserAdapter(RecipientsActivity.this, mFriends);
+                        mGridView.setAdapter(adapter);
+                    }
+                    else {
+                        ((UserAdapter)mGridView.getAdapter()).refill(mFriends);
+                    }
+
                 } else {
 
                     Log.e(TAG, e.getMessage());
@@ -134,18 +149,6 @@ public class RecipientsActivity extends ListActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (l.getCheckedItemCount() > 0){
-            mSendMenuItem.setVisible(true);
-        }
-        else {
-        mSendMenuItem.setVisible(true);
-        }
-    }
-
     protected ParseObject createMessage(){
         ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
         message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
@@ -172,8 +175,8 @@ public class RecipientsActivity extends ListActivity{
 
     protected ArrayList<String> getRecipientIds(){
         ArrayList<String> recipientIds = new ArrayList<String>();
-        for (int i = 0; i < getListView().getCount(); i++){
-            if (getListView().isItemChecked(i)){
+        for (int i = 0; i < mGridView.getCount(); i++){
+            if (mGridView.isItemChecked(i)){
                 recipientIds.add(mFriends.get(i).getObjectId());
             }
         }
@@ -199,4 +202,28 @@ public class RecipientsActivity extends ListActivity{
             }
         });
     }
+
+    protected AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            if (mGridView.getCheckedItemCount() > 0){
+                mSendMenuItem.setVisible(true);
+            }
+            else {
+                mSendMenuItem.setVisible(true);
+            }
+
+            ImageView checkImageView = (ImageView)findViewById(R.id.checkImageView);
+
+            if(mGridView.isItemChecked(position)){
+                //add the recipient
+                checkImageView.setVisibility(View.VISIBLE);
+            }
+            else {
+                //remove the recipient
+                checkImageView.setVisibility(View.INVISIBLE);
+            }
+        }
+
+    };
 }
