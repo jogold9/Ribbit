bf6f1f5ec6faa534e81b9e8d78c13cffcf374f54
joshgoldeneagle <joshuabgold@gmail.com>
2015-09-15 12:24:07 -0700

    -added push notifications capability from parse backend

--- app/src/main/java/com/joshbgold/ribbit/RibbitApplication.java
+++ app/src/main/java/com/joshbgold/ribbit/RibbitApplication.java
@@ -3,6 +3,7 @@ package com.joshbgold.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 public class RibbitApplication extends Application {
 
@@ -12,6 +13,7 @@ public class RibbitApplication extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "W2tNAzL0KmSX6D2KzPrgXj6xVWmDHhyU0oeCuqAd", "31zsSt6SutjBLLnJVgrPith4XyGwHbPSsQTjF4wZ");
+        ParseInstallation.getCurrentInstallation().saveInBackground();  //saves device OS information to background on Parse.com
 
      /* //For testing
         ParseObject testObject = new ParseObject("TestObject");
